1f82e93f74ca1001801f56cfa22ae7ed4d3ffbe7
rina redis <marinaredka1@gmail.com>
2021-09-08 23:29:16 +0300

    Boolean part

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Abramyan
     {
         static void Main(string[] args)
         {
-            Integer30(1901);
+            Console.WriteLine(Boolean10(3, 3)); 
         }
         public static void Integer7(int x)
         {
@@ -78,6 +78,111 @@ namespace Abramyan
         {
             int century = (year-1)/ 100 + 1;            
             Console.WriteLine(century);
+        }
+
+
+        public bool Boolean2(int a)
+        {
+            return a % 2 != 0;
+        }
+        public bool Boolean4(int a, int b)
+        {
+            return (a > 2 && b <= 3);
+        }
+        public bool Boolean6(int a, int b, int c)
+        {
+            return (b > a && c > b);
+        }
+        public bool Boolean8(int a, int b)
+        {
+            return a % 2 != 0 && b % 2 != 0;
+        }
+        public static bool Boolean10(int a, int b)
+        {
+            return a % 2 != 0 ^ b % 2 != 0;
+        }
+        public static bool Boolean12(int a, int b, int c)
+        {
+            return a % 2 != 0 && b % 2 != 0 && c % 2 != 0;
+        }
+        public static bool Boolean14(int a, int b, int c)
+        {
+            return a > 0 ^ b > 0 ^ c > 0;
+        }
+        public static bool Boolean16(int a)
+        {
+            return a % 2 == 0 && a / 10 > 0;
+        }
+        public static bool Boolean18(int a, int b, int c)
+        {
+            return a == b || b == c || a == c;
+        }
+        public static bool Boolean19(int a, int b, int c)
+        {
+            return a == -b || b == -c || a ==-c;
+        }
+        public static bool Boolean20(int a)
+        {
+            int hundrets = a / 100;
+            int tens = (a % 100) / 10;
+            int ones = a % 10;
+            return hundrets != tens && tens != ones && hundrets != ones;
+        }
+        public static bool Boolean22(int a)
+        {
+            int hundrets = a / 100;
+            int tens = (a % 100) / 10;
+            int ones = a % 10;
+            return (hundrets > tens && tens > ones) || (hundrets < tens && tens < ones);
+        }
+        public static bool Boolean24(int a, int b, int c)
+        {
+            float discriminant = (b * b) - (4 * a * c);
+            return discriminant > 0;
+        }
+        public static bool Boolean26(int x, int y)
+        {
+            return x > 0 && y < 0;
+        }
+        public static bool Boolean28(int x, int y)
+        {
+            return (x > 0 && y > 0) || (x < 0 && y < 0);
+        }
+        public static bool Boolean29(int x, int y, int x1, int y1, int x2, int y2)
+        {
+            return x>x1 && x < x2 && y > y1 && y < y2;
+        }
+        public static bool Boolean30(int a, int b, int c)
+        {
+            return a==b&&b==c;
+        }
+        public static bool Boolean32(int a, int b, int c)
+        {
+            return (c * c == a * a + b * b) || (a * a == b * b + c * c) || (b * b == a * a + c * c);
+        }
+        public static bool Boolean34(int x, int y)
+        {
+            return (x + y) % 2 != 0; //why?
+        }
+        public static bool Boolean35(int x1, int y1, int x2, int y2)
+        {
+            return (x1 + y1) % 2 == (x2 + y2) % 2;
+        }
+        public static bool Boolean36(int x1, int y1, int x2, int y2)
+        {
+            return x1==x2 || y1==y2;
+        }
+        public static bool Boolean37(int x1, int y1, int x2, int y2)
+        {
+            return Math.Abs(x1 - x2) == 1 || Math.Abs(y1 - y2) == 1;
+        }
+        public static bool Boolean38(int x1, int y1, int x2, int y2)
+        {
+            return Math.Abs(x1 - x2) ==  Math.Abs(y1 - y2);
+        }
+        public static bool Boolean40(int x1, int y1, int x2, int y2)
+        {
+            return (Math.Abs(x1 - x2) == 2 && Math.Abs(y1 - y2) == 1) || (Math.Abs(x1 - x2) == 1 && Math.Abs(y1 - y2) == 2);
         }
     }
 }
